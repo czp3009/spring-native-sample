10c9f735a645cf783ad54aeb5ded7450daf8cde8
czp3009 <czp3009@gmail.com>
2021-12-17 18:13:17 +0800

    init

---
+++ src/main/kotlin/org/example/Application.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main() {
+    runApplication<Application>()
+}
